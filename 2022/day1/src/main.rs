b1b151e9339302980fcd9744630087d14eeea34b
Dāvis Krēgers <daviskregers@protonmail.com>
2022-12-03 14:01:43 +0200

    feat: day 1 complete

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -8,4 +8,15 @@ fn main() {
     let input = fs::read_to_string("input").expect("Should have been able to read the file");
     let parsed = parser::part1_most_calories(&input);
     println!("The Elf number {:?} has the most calories: {:?}", parsed[0], parsed[1]);
+
+    println!("PART2:");
+    let parsed = parser::part2_top_calories(&input);
+
+    println!("Elf top:");
+    let mut total : usize = 0;
+    for elf in parsed {
+        total += elf[1];
+        println!("Elf NR: {:?}: {:?}", elf[0], elf[1]);
+    }
+    println!("Total for those elves: {:?}", total);
 }
--- 2022/day1/src/parser.rs
+++ 2022/day1/src/parser.rs
@@ -1,3 +1,5 @@
+use indexmap::IndexMap;
+
 pub fn part1_most_calories(input: &String) -> [usize; 2]
 {
     let mut total_max : usize = 0;
@@ -24,9 +26,34 @@ pub fn part1_most_calories(input: &String) -> [usize; 2]
     return [total_index + 1, total_max];
 }
 
+pub fn part2_top_calories(input: &String) -> [[usize; 2]; 3]
+{
+    let lines = input.split("\n");
+    let mut current_index : usize = 0;
+    let mut top = IndexMap::new();
+
+    for item in lines {
+        if item.is_empty() {
+            current_index += 1;
+            continue;
+        }
+        let units = item.parse::<usize>();
+        *top.entry(current_index).or_insert(0) += units.unwrap();
+    }
+
+    top.sort_by(|_, av, _, bv| av.cmp(&bv));
+
+    let mut result : [[usize; 2]; 3] = [[0; 2]; 3];
+    for i in 0..3 {
+        let record = top.pop().unwrap();
+        result[i] = [record.0 + 1, record.1];
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::parser::part1_most_calories;
+    use crate::parser::{part1_most_calories, part2_top_calories};
 
     const TEST_INPUT : &str = "1000
 2000
@@ -47,6 +74,16 @@ mod tests {
     fn finds_most_calories() {
         let actual = part1_most_calories(&String::from(TEST_INPUT));
         let expected : [usize; 2] = [4, 24000];
+
+        println!("{:?} == {:?}", actual, expected);
+        assert_eq!(actual.len(), expected.len(), "Arrays don't have the same length");
+        assert!(actual.iter().zip(expected.iter()).all(|(a,b)| a == b), "Arrays are not equal");
+    }
+
+    #[test]
+    fn finds_top3_calories() {
+        let actual = part2_top_calories(&String::from(TEST_INPUT));
+        let expected : [[usize; 2]; 3] = [[4, 24000], [3, 11000], [5, 10000]];
 
         println!("{:?} == {:?}", actual, expected);
         assert_eq!(actual.len(), expected.len(), "Arrays don't have the same length");
